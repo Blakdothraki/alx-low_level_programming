bb26a354c478b05c018c58c237f9109c4b8ba286
Blakdothraki <blakdothraki@github.com>
2022-06-04 11:03:33 -0700

    advanced tasks done

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,7 +12,7 @@ int main(void)
 	{
 		for (n2 = n1 + 1; n2 < 9; n2++)
 		{
-			for (n3 = n2 +1; n3 < 10; n3++)
+			for (n3 = n2 + 1; n3 < 10; n3++)
 			{
 				putchar((n1 % 10) + '0');
 				putchar((n2 % 10) + '0');
