811b7ec9602362ba043229c2c8a8f08f771cf79c
Blakdothraki <blakdothraki@github.com>
2022-06-07 20:13:03 -0700

    mandatory

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,20 @@
-#include "main.h"
 /**
  *Header file created by blakdothraki
  * A function that prints alphabets in lower case
  */
+
+#include "main.h"
+/**
+ * Header file created by blakdothraki
+ * A function that prints alphabets in lower case
+ */
 void print_alphabet(void)
-{
+/**
+ * void print_alphabet(void)
+ * prints alphabet
+ */{
 	char alpha;
+
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
 	{
 		_putchar(alpha);
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,9 +7,11 @@
 void print_alphabet_x10(void)
 {
         int count = 0;
+
         while (count < 10)
 	{
 		char alpha;
+
 		for (alpha = 'a'; alpha <= 'z'; alpha++)
 		{
 			_putchar(alpha);
