5f40e2b21310edad70350efe1c6be5bf129b27f5
Blakdothraki <blakdothraki@github.com>
2022-06-24 18:16:59 +0100

    done

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,5 +12,35 @@
  */
 int main (int argc, char *argv[])
 {
+    int unit[5] = {25, 10, 5, 2, 1};
+    int temp, i;
+    temp = 0;
+    int value = atoi(argv[1]);
 
+    if (argc == 2)
+    {
+        if (value <= 0)
+            printf("0\n");
+        else
+        {
+            for (i = 0; i < 5; i++)
+            {
+                temp = value / unit[i];
+                value -= temp * unit[i];
+                if (value == 0)
+                {
+                    printf("%d\n", temp);
+                    break;
+                }
+            }
+        }
+    }
+    else
+    {
+        printf("Error\n");
+        return (1);
+    }
+
+    return (0);
 }
+
