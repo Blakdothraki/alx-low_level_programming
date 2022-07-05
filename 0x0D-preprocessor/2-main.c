3f9224454fd7be49af29110d81f63c80dd50d47a
Blakdothraki <blakdothraki@github.com>
2022-07-05 02:25:30 +0100

    fixed

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,12 @@
- #include <stdio.h>
+#include <stdio.h>
+
 /**
- * main - function called
- *
- * Descritption: ...
- * Return: ...
- */
+  * main - Prints the file name of the program
+  *
+  * Return: Always zero
+  */
 int main(void)
 {
-	printf("%s/n", __FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
